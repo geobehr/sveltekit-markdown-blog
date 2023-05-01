0ca82b2d6fbd7b847a9a906e90559cf74385caba
Geo Behr <geo@behrs.org>
2023-05-01 12:30:19 -0400

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment'
 
-export const title = 'Shakespeare'
-export const description = 'SvelteKit blog for poets'
-export const url = dev ? 'http://localhost:5173' : 'https://joyofcode.xyz'
+export const title = 'Behr Log'
+export const description = 'SvelteKit Blog for Behrs'
+export const url = dev ? 'http://localhost:5173' : 'https://behrs.org'
